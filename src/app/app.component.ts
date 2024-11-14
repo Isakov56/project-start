704881dab56cee433e3743c96800dc9774d84092
Isakov Khojiakbarkhon <72197872+Isakov56@users.noreply.github.com>
2024-11-14 17:12:11 +0100

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { routes } from './app.routes';
+import { SharedLayoutComponent } from 'shared-layout';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SharedLayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'project-start';
+  public routes = routes;
 }
